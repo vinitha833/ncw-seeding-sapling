d36b8c7fc9de17edfd479e1225325c140a73ce45
vinitha833 <99422256+vinitha833@users.noreply.github.com>
2022-03-17 08:06:05 +0530

    name changes

--- header.php
+++ header.php
@@ -11,7 +11,7 @@ require 'connection.php';
 
 <head>
     <!-- <link rel="shortcut icon" href="img/seeding-slaping-1.jpeg" /> -->
-    <title>NCW SEEDING AND SLAPING</title>
+    <title>Virtual outlet site</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- latest compiled and minified CSS -->
